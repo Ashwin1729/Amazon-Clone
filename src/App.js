95aba30a197f8e7af0df5250e92b7a5b63673cca
Ashwin1729 <mathematicswroteus@gmail.com>
2023-01-08 15:28:20 +0530

    payment initiation using stripe.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ import Login from "./components/Login";
 import { StateContext } from "./store/StateProvider";
 import { auth } from "./firebase";
 import Payment from "./components/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51MNqeCSHaCvRvJ1rTlh5OdekImjl9vrb2QhswQjovr9HBYsyDJ9CFvFMUO92LM5TTW8jIItBUotbw4EI51nqssO900Q8XA22rv"
+);
 
 function App() {
   const [appData, dispatchAction] = useContext(StateContext);
+
+  // const options = {
+  //   clientSecret:
+  //     "{{sk_test_51MNqeCSHaCvRvJ1rLIaTcuYnuUQfb8JZ21LusrYWEl84H1XE4VvV133ilvA8p5TP7JBWzePs5mWpzHXE6Tw9XXuy00JGZ9HMuZ}}",
+  // };
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
@@ -51,7 +62,9 @@ function App() {
             element={
               <div>
                 <Header />
-                <Payment />
+                <Elements stripe={stripePromise}>
+                  <Payment />
+                </Elements>
               </div>
             }
           />
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,12 +1,76 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "./axios";
+import { Link, useNavigate } from "react-router-dom";
 import { StateContext } from "../store/StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 import FlipMove from "react-flip-move";
 import styles from "./Payment.module.css";
+import CurrencyFormat from "react-currency-format";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 const Payment = () => {
   const [appData, dispatchAction] = useContext(StateContext);
+
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  useEffect(() => {
+    // generate special client secret for client payment processing
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "POST",
+        // stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${clacTotalAmount(appData.basket) * 100}`,
+      });
+
+      setClientSecret(response.data.clientSecret);
+      console.log("Client Secret", response.data);
+    };
+
+    getClientSecret();
+  }, [appData.basket]);
+
+  const clacTotalAmount = (items, prop) => {
+    return items.reduce((acc, curr) => acc + curr[prop], 0);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setProcessing(true);
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    });
+
+    const response = payload.paymentIntent;
+
+    if (response) {
+      setSucceeded(true);
+      setError(null);
+      setProcessing(false);
+    }
+
+    navigate("/orders");
+  };
+
+  const elementChangeHandler = (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   return (
     <div className={styles.payment}>
@@ -53,7 +117,35 @@ const Payment = () => {
           <div className={styles.payment__title}>
             <h3>Payment Method</h3>
           </div>
-          <div className={styles.payment__details}>{/* Stripe Magic */}</div>
+          <div className={styles.payment__details}>
+            {/* Stripe Magic */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={elementChangeHandler} />
+              <div className={styles.payment__priceContainer}>
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <div>
+                        ................................................
+                      </div>
+                      <h4>Order Total: {value}</h4>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={clacTotalAmount(appData.basket, "price")}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+
+                {/* Errors */}
+                {error && <div>{error}</div>}
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
